90c0ffda72f75bc66fc3b1bcadf3802315cef704
Vijay N <101880078+Vijayn25@users.noreply.github.com>
2022-11-08 09:45:10 +0530

    Create full_adder.v

---
+++ full_adder.v
@@ -0,0 +1,14 @@
+//
+module full_add(
+    input a,
+    input b,
+    input c,
+    output reg sum,
+    output reg carry
+    );
+    always @(*)
+    begin
+    sum = a^b^c;
+    carry = (a&b)|(a&c)|(b&c);
+    end
+endmodule
