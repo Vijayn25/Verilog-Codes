b5def5f63f36c9fe3f8422a64db2528fbd349e85
Vijay N <101880078+Vijayn25@users.noreply.github.com>
2022-11-08 09:34:42 +0530

    Create half_adder.v

---
+++ half_adder.v
@@ -0,0 +1,14 @@
+
+
+module half_adder(
+    input a,
+    input b,
+    output reg sum,
+    output reg carry
+    );
+    always @(*)
+    begin 
+    sum = a ^ b;
+    carry = a & b;
+    end
+endmodule
